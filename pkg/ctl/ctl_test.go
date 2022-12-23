4505aaf269dfc31b55ad8f1f82a5e638ec7b952f
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-12-23 12:30:14 -0600

    Add initial vex merge test and fixtures
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/ctl/ctl_test.go
+++ pkg/ctl/ctl_test.go
@@ -27,3 +27,56 @@ func TestVexReport(t *testing.T) {
 	require.Len(t, newReport.Runs, 1)
 	require.Len(t, newReport.Runs[0].Results, 122)
 }
+
+func TestMerge(t *testing.T) {
+	doc1, err := vex.Load("testdata/document1.vex.json")
+	require.NoError(t, err)
+	doc2, err := vex.Load("testdata/document1.vex.json")
+	require.NoError(t, err)
+
+	impl := defaultVexCtlImplementation{}
+	for _, tc := range []struct {
+		opts        MergeOptions
+		docs        []*vex.VEX
+		expectedDoc *vex.VEX
+		shouldErr   bool
+	}{
+		// CeZero docs should fail
+		{
+			opts:        MergeOptions{},
+			docs:        []*vex.VEX{},
+			expectedDoc: &vex.VEX{},
+			shouldErr:   true,
+		},
+		// One doc results in the same doc
+		{
+			opts:        MergeOptions{},
+			docs:        []*vex.VEX{doc1},
+			expectedDoc: doc1,
+			shouldErr:   false,
+		},
+		// Two docs, as they are
+		{
+			opts: MergeOptions{},
+			docs: []*vex.VEX{doc1, doc2},
+			expectedDoc: &vex.VEX{
+				Metadata: vex.Metadata{},
+				Statements: []vex.Statement{
+					doc1.Statements[0],
+					doc2.Statements[0],
+				},
+			},
+			shouldErr: false,
+		},
+	} {
+		doc, err := impl.Merge(tc.opts, tc.docs)
+		if tc.shouldErr {
+			require.Error(t, err)
+			continue
+		}
+
+		// Check doc
+		require.Len(t, doc.Statements, len(tc.expectedDoc.Statements))
+		require.Equal(t, doc.Statements, tc.expectedDoc.Statements)
+	}
+}
