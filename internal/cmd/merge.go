c14b495faa22ce117d711e81fa6779bbc054a781
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-12-23 14:37:09 -0600

    merge subcommand!
    This commit adds a merge subcommand to vexctl. For now it only works with
    our simple vex format bur we have the import code necessary to work with
    the rest of them.
    
    Using merge, multiple documents can be combined into one, capturing all
    known impact data in one doc.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ internal/cmd/merge.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2022 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"chainguard.dev/vex/pkg/ctl"
+)
+
+type mergeOptions struct {
+	ctl.MergeOptions
+}
+
+func addMerge(parentCmd *cobra.Command) {
+	opts := mergeOptions{}
+	mergeCmd := &cobra.Command{
+		Short: fmt.Sprintf("%s merge: merges two or more VEX documents into one", appname),
+		Long: fmt.Sprintf(`%s merge: merge one or more documents into one
+
+When composing VEX data out of multiple sources it may be necessary to mix
+all statements into a single doc. The merge subcommand mixes the statements
+from one or more vex documents into a single, new one.
+
+Examples:
+
+# Merge two documents into one
+%s merge document1.vex.json document2.vex.json > new.vex.json
+
+# Merge two documents into one, but only one product
+%s merge --product="pkg:apk/wolfi/bash@1.0" document1.vex.json document2.vex.json 
+
+# Merge vulnerability data from two documents into one
+%s merge --vulnerability=CVE-2022-3294 document1.vex.json document2.vex.json 
+
+`, appname, appname, appname, appname),
+		Use:               "merge",
+		SilenceUsage:      false,
+		SilenceErrors:     false,
+		PersistentPreRunE: initLogging,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			vexctl := ctl.New()
+			newVex, err := vexctl.MergeFiles(context.Background(), &opts.MergeOptions, args)
+			if err != nil {
+				return fmt.Errorf("merging documents: %w", err)
+			}
+			if err := newVex.ToJSON(os.Stdout); err != nil {
+				return fmt.Errorf("writing new vex document: %w", err)
+			}
+			return nil
+		},
+	}
+
+	mergeCmd.PersistentFlags().StringSliceVar(
+		&opts.Vulnerabilities,
+		"vuln",
+		[]string{},
+		"list of vulnerabilities to extract",
+	)
+
+	mergeCmd.PersistentFlags().StringSliceVar(
+		&opts.Products,
+		"product",
+		[]string{},
+		"list of products to merge, all others will be ignored",
+	)
+
+	parentCmd.AddCommand(mergeCmd)
+}
