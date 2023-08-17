4a435bb28a625759085ba80451625e8ff42aa29e
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-08-16 22:26:44 -0600

    Revamp tests for v0.2.2 add more fixtures
    This commit udpates and improves the project's tests to
    work with the new spec v0.2.0. It also adds various new
    documents to widen the test cases and ensure compatibility
    with the previous spec version.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/ctl/ctl_test.go
+++ pkg/ctl/ctl_test.go
@@ -6,7 +6,6 @@ SPDX-License-Identifier: Apache-2.0
 package ctl
 
 import (
-	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,74 +15,44 @@ import (
 )
 
 func TestVexReport(t *testing.T) {
-	vexDoc, err := vex.OpenJSON("testdata/test.vex.json")
-	require.NoError(t, err)
-	require.NotNil(t, vexDoc)
-	require.Len(t, vexDoc.Statements, 2)
-
-	report, err := sarif.Open("testdata/nginx.sarif.json")
-	require.NoError(t, err)
-	require.NotNil(t, report)
-	require.Len(t, report.Runs, 1)
-	require.Len(t, report.Runs[0].Results, 123)
-
-	impl := defaultVexCtlImplementation{}
-	newReport, err := impl.ApplySingleVEX(report, vexDoc)
-	require.NoError(t, err)
-	require.Len(t, newReport.Runs, 1)
-	require.Len(t, newReport.Runs[0].Results, 122)
-}
-
-func TestMerge(t *testing.T) {
-	ctx := context.Background()
-	doc1, err := vex.Load("testdata/document1.vex.json")
-	require.NoError(t, err)
-	doc2, err := vex.Load("testdata/document1.vex.json")
-	require.NoError(t, err)
-
 	impl := defaultVexCtlImplementation{}
 	for _, tc := range []struct {
-		opts        MergeOptions
-		docs        []*vex.VEX
-		expectedDoc *vex.VEX
-		shouldErr   bool
+		vexDoc         string
+		lenStatements  int
+		sarifDoc       string
+		lenRuns        int
+		lenResults     int
+		lenAfterFilter int
 	}{
-		// Zero docs should fail
-		{
-			opts:        MergeOptions{},
-			docs:        []*vex.VEX{},
-			expectedDoc: &vex.VEX{},
-			shouldErr:   true,
-		},
-		// One doc results in the same doc
-		{
-			opts:        MergeOptions{},
-			docs:        []*vex.VEX{doc1},
-			expectedDoc: doc1,
-			shouldErr:   false,
-		},
-		// Two docs, as they are
-		{
-			opts: MergeOptions{},
-			docs: []*vex.VEX{doc1, doc2},
-			expectedDoc: &vex.VEX{
-				Metadata: vex.Metadata{},
-				Statements: []vex.Statement{
-					doc1.Statements[0],
-					doc2.Statements[0],
-				},
-			},
-			shouldErr: false,
-		},
-	} {
-		doc, err := impl.Merge(ctx, &tc.opts, tc.docs)
-		if tc.shouldErr {
-			require.Error(t, err)
-			continue
-		}
+		// One OpenVEX statement, filters one vuln
+		{"testdata/sarif/sample.openvex.json", 1, "testdata/sarif/nginx-grype.sarif.json", 1, 99, 98},
+		{"testdata/sarif/sample.openvex.json", 1, "testdata/sarif/nginx-trivy.sarif.json", 1, 99, 98},
+		{"testdata/sarif/sample.openvex.json", 1, "testdata/sarif/nginx-snyk.sarif.json", 2, 65, 64},
 
-		// Check doc
-		require.Len(t, doc.Statements, len(tc.expectedDoc.Statements))
-		require.Equal(t, doc.Statements, tc.expectedDoc.Statements)
+		// Two OpenVEX statements, filters one vuln
+		{"testdata/sarif/sample-history.json", 2, "testdata/sarif/nginx-grype.sarif.json", 1, 99, 98},
+		{"testdata/sarif/sample-history.json", 2, "testdata/sarif/nginx-trivy.sarif.json", 1, 99, 98},
+		{"testdata/sarif/sample-history.json", 2, "testdata/sarif/nginx-snyk.sarif.json", 2, 65, 64},
+
+		// Two OpenVEX statements, filters two vuln
+		{"testdata/sarif/sample-2vulns.json", 2, "testdata/sarif/nginx-grype.sarif.json", 1, 99, 96},
+		{"testdata/sarif/sample-2vulns.json", 2, "testdata/sarif/nginx-trivy.sarif.json", 1, 99, 96},
+		{"testdata/sarif/sample-2vulns.json", 2, "testdata/sarif/nginx-snyk.sarif.json", 2, 65, 63},
+	} {
+		vexDoc, err := vex.Open(tc.vexDoc)
+		require.NoError(t, err)
+		require.NotNil(t, vexDoc)
+		require.Len(t, vexDoc.Statements, tc.lenStatements)
+
+		report, err := sarif.Open(tc.sarifDoc)
+		require.NoError(t, err)
+		require.NotNil(t, report)
+		require.Len(t, report.Runs, tc.lenRuns)
+		require.Len(t, report.Runs[0].Results, tc.lenResults)
+
+		newReport, err := impl.ApplySingleVEX(report, vexDoc)
+		require.NoError(t, err)
+		require.Len(t, newReport.Runs, tc.lenRuns)
+		require.Len(t, newReport.Runs[0].Results, tc.lenAfterFilter)
 	}
 }
